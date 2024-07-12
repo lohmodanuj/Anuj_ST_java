fb422fcc7853eaae71737ee02fc0d64451a18339
Anuj Lohia <annul00weak@gmail.com>
2024-07-12 14:07:56 +0530

    day2

--- Week0/Day2/patternprinting.java
+++ Week0/Day2/patternprinting.java
@@ -261,21 +261,7 @@ public class patternprinting {
 
         //Ques16
         
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i=1; i<=n; i++){
-            for (int j=1; j<=i; j++)
-                System.out.print("* ");
-            System.out.println();
-        }
-        for (int i=2; i<=n; i++){
-            for(int k=1; k<=(n-i); k++)
-                System.out.print("");
-            for (int j=1; j<=i; j++)
-                System.out.print("* ");
-            System.out.println();
-        }
-        sc.close();
+        
     }
     
 }
